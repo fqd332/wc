3ac9b46533645fb2a9157fb891a61a50a448b3c2
Fanqidong <274750178@qq.com>
2018-03-20 21:33:29 +0800

    Add files via upload

---
+++ BIN/wc.bat
@@ -0,0 +1,11 @@
+@echo off
+wc.exe -c  -o result.txt
+wc.exe -a  -o result.txt
+wc.exe -w -e  -o result.txt
+wc.exe -c -w -l  -o result.txt
+wc.exe -c -c -c  -o result.txt
+wc.exe -w -c -e  -o result.txt
+wc.exe -c -s -o result.txt
+wc.exe -l -s -o result.txt
+wc.exe -w -e -s -o result.txt
+wc.exe -c -w -l -s -o result.txt
